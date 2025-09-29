f1141074a4a9b78cefe6816cce3245359107c3b0
Cazhed <zachary.sechan@gmail.com>
2025-09-29 14:40:28 +0700

    Shape & Circle Class

--- src/week05/zachary/id/ac/umn/Shape.java
+++ src/week05/zachary/id/ac/umn/Shape.java
@@ -1,10 +1,17 @@
 package week05.zachary.id.ac.umn;
 
 public class Shape {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String color;
+	
+	public Shape () {}
+	public Shape(String color) {
+		this.color = color;
 	}
-
+	
+	public void setColor(String color) {this.color= color;}
+	
+	public String getColor() {return color; }
+	
+	public double getAre() {return 0;}
+	public double getPerimeter() {return 0;}
 }
